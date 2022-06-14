67a2fa9604e4c5cebc6f69ff548fb2b01f19be62
rebeca.prado <rebeca.2003.prado@gmail.com>
2022-06-14 11:38:15 -0300

    refactor/interação com interface gráfica

--- js/index.js
+++ js/index.js
@@ -9,7 +9,7 @@ const limparElementos = (nameEmelemto) => {
 };
 
 const ListarPais = async () => {
-  limparElementos('#cardPalavra')
+  limparElementos("#cardPalavra");
   const urlListar = `https://servicodados.ibge.gov.br/api/v1/localidades/paises?orderBy=nome`;
   const options = {
     method: "GET",
@@ -17,15 +17,8 @@ const ListarPais = async () => {
 
   await fetch(urlListar, options)
     .then((resp) => resp.json())
-    .then((json) => listaNomePais(json))
+    .then((json) => palavraGerada(json))
     .catch((err) => console.log(err));
-};
-
-
-
-
-const clickMe = () => {
- 
 };
 
 const listAlphabet = () => {
@@ -57,98 +50,120 @@ const listAlphabet = () => {
     "Y",
     "Z",
   ];
-  
+
   return letter.map((item) => exibirAlfabeto(item));
 };
-
-
 
 const exibirAlfabeto = (letra) => {
   const container = document.querySelector("#cardLetra");
   const div = document.createElement("div");
 
-  div.classList.add('letras')
+  div.classList.add("letras");
   div.id = letra;
   div.innerHTML = letra;
-  div.onclick = verificarLetraNomePais;
+  div.onclick = existeNaPalavra;
 
   container.appendChild(div);
 };
 
-const exibirPalavra = (caractere) => {
+const substituirPalavraUnderline = () => {
   const container = document.querySelector("#cardPalavra");
   const div = document.createElement("p");
 
-  div.classList.add('caractere')
-  div.innerHTML = caractere ?? '_';
-  div.onclick = verificarLetraNomePais;
+  div.classList.add("caractere");
+  div.innerHTML = "_";
 
   container.appendChild(div);
 };
 
-// const exibirPaisEscolhido = (pais) => {
-//   const container = document.querySelector("#cardPalavra");
-//   const div = document.createElement("p");
+let arrayPalavra;
 
-//   div.innerHTML = pais;
-  
-//   container.appendChild(div);
-// };
+let posicaoLetra = [];
 
+let letrasDaPalavra = [];
 
+let replace = [];
+var indices = [];
 
-let pais;
+const formarPalavra = () => {
+  limparElementos("#cardPalavra");
+  const replace = arrayPalavra.map((caractere) => (caractere = "_"));
+  indices.map(({ indice, letra }) => (replace[indice] = letra));
+
+  replace.map((caractere) => exibirLetraDaPalavra(caractere));
+};
+
+const exibirLetraDaPalavra = (letra) => {
+  const container = document.querySelector("#cardPalavra");
+  const div = document.createElement("p");
+
+  div.classList.add("caractere");
+  div.innerHTML = letra;
+
+  container.appendChild(div);
+};
+
+console.log(arrayPalavra);
+
+const existeNaPalavra = (e) => {
+  const letra = e.target.id;
+
+  const verificar = arrayPalavra.filter(
+    (item) => item == letra.toUpperCase() || item == letra.toLowerCase()
+  );
+
+  if (verificar.length != 0) {
+    var idx = arrayPalavra.indexOf(verificar[0]);
+
+    while (idx != -1) {
+      indices.push({ indice: idx, letra: letra });
+      // posicaoLetra.push({indice: idx, letra: letra})
+      idx = arrayPalavra.indexOf(verificar[0], idx + 1);
+    }
+
+    console.log("indices =>", indices);
+    letrasDaPalavra.push(letra);
+
+    console.log("letrasDaPalavra", letrasDaPalavra);
+    console.log(arrayPalavra);
+
+    formarPalavra();
+    // exibirLetraDaPalavra(letra, indices);
+
+    // const exibirLetra = indices.map((item) => (replace[item] = letra));
+  } else {
+    console.log("tem essa letra não");
+  }
+};
 
 function numeroAleatorio(max) {
   console.log(Math.floor(Math.random() * max + 1));
   return Math.floor(Math.random() * max + 1);
 }
 
-const listaNomePais = (json) => {
+const palavraGerada = (json) => {
   let arrayNomePais = json.map((obj) => obj.nome);
 
   let numero = numeroAleatorio(arrayNomePais.length);
 
   let nomePais = arrayNomePais
     .filter((_item, index) => index === numero)
-    .toString();
+    .toString().normalize("NFD");
 
-  pais = nomePais.split("");
-  pais.map((item) => (item == ' ' || item == '-') ? exibirPalavra('-') : exibirPalavra());
+
+
+
+  arrayPalavra = nomePais.split("");
+
+  arrayPalavra = arrayPalavra.map((item) => item.toUpperCase());
+
+  arrayPalavra.map((letra) => substituirPalavraUnderline(letra));
+
+  // pais.map((item) => (item == ' ' || item == '-') ? exibirPalavra('-') : exibirPalavra());
 };
-
-console.log(pais)
-
-const verificarLetraNomePais = (e) => {
-  const replace = pais.map((item) => (item = "_"));
-  const letra = e.target.id
-  console.log('letra', letra)
-  const vericar = pais.filter((item, index) => item == letra);
-
-  if (vericar.length != 0) {
-    var idx = pais.indexOf(vericar[0]);
-    var indices = [];
-
-    while (idx != -1) {
-      indices.push(idx);
-      idx = pais.indexOf(vericar[0], idx + 1);
-    }
-
-    const exibirLetra = indices.map((item) => (replace[item] = letra));
-
-    let abc = pais.indexOf(vericar[0]);
-    console.log(pais);
-    console.log(replace);
-    console.log('exiba', exibirLetra);
-  } else {
-    console.log("tem essa letra não");
-  }
-};
-
-const exibirPais = () => {};
 
 document
   .getElementById("btnPalavraAleatoria")
   .addEventListener("click", ListarPais);
-  document.addEventListener("DOMContentLoaded", listAlphabet);
-  document.addEventListener("DOMContentLoaded", ListarPais);
+document.addEventListener("DOMContentLoaded", listAlphabet);
+document.addEventListener("DOMContentLoaded", ListarPais);
